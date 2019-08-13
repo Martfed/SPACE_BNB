5fd1d229a4005d8c4b76619ec3cc8fef1f22f97a
Edouard De Prez <deprezedouard@gmail.com>
2019-08-13 15:11:59 +0200

    Revert "added profile page for spaceship"
    This reverts commit abd4b5be45f555c73f099ba356a01d097b654e76.

--- app/controllers/spaceships_controller.rb
+++ app/controllers/spaceships_controller.rb
@@ -1,6 +1,5 @@
 class SpaceshipsController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
-  before_action :set_spaceship, only: [:show]
 
   def index
     @spaceships = Spaceship.all
@@ -22,11 +21,5 @@ class SpaceshipsController < ApplicationController
   end
 
   def destroy
-  end
-
-  private
-
-  def set_spaceship
-    @spaceship = Spaceship.find(params[:id])
   end
 end
