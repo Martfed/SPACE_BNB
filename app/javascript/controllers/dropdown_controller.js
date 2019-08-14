d1ebf1fb6b66de6896b4c12115bb9953b6527930
Federico De Martino <federicodm90@gmail.com>
2019-08-14 15:03:41 +0200

    refactoring in progress

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -67,5 +67,9 @@ export default class extends Controller {
       (this.results_done_bookingsTarget).classList.remove("donebookings");
      });
   }
+
+  show() {
+    console.log("Hello")
+  }
 }
 
