6ba4e24846a35dd6b632d5e1794ac31d1650356a
Federico De Martino <federicodm90@gmail.com>
2019-08-14 10:45:46 +0200

    My bookings

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:user_id])
+  end
+
+  def myspaceships
+    @bookings = Booking.where(user_id: params[:user_id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root to: 'spaceships#index'
 
   resources :users do
+    get "dashboard", to: "users#myspaceships"
     resources :spaceships, only: [:index, :new, :edit, :show, :create, :update, :destroy] do
       resources :reviews
       resources :bookings, only: [:index, :new, :edit, :create]
