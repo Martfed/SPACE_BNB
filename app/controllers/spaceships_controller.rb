0a352a4442b4ffa535f628d500f2c74d651c5bd0
Francois-Stevenart <f.stevenart@gmail.com>
2019-08-14 10:44:17 +0200

    alerts notifications done

--- app/controllers/spaceships_controller.rb
+++ app/controllers/spaceships_controller.rb
@@ -1,5 +1,5 @@
 class SpaceshipsController < ApplicationController
-  skip_before_action :authenticate_user!, only: :index
+  skip_before_action :authenticate_user!, only: [:index]
   before_action :set_spaceship, only: [:show, :edit, :update]
 
   def index
